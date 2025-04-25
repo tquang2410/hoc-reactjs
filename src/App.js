6560b4603b9b59dcf0af58672c1ad791a773efce
Ta Quang <justicepencil@gmail.com>
2025-04-25 17:39:23 +0700

    Hoc component

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ export default class App extends React.Component {
   render () {
     return (
       <div>
-      
         <MyComponent></MyComponent>
       </div>
     )
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,44 +1,12 @@
 import React from 'react';
+import UserInfor from './UserInfor';
 export default class MyComponent extends React.Component {
-    state = {
-        name: 'Quang',
-        address: 'Nhà mèo',
-        age: 24
-    };
-    handleClick = (event) =>{
-          
-            // console.log("Random", Math.floor((Math.random()*100) +1) )
-            this.setState({
-            
-                age: Math.floor((Math.random()*100) +1)
-            })
-          
-    }
-    handleOnMouseOver(event)
-    {
-        // console.log(event)
-    }
-    handleOnChangeInput = (event) => {
-        this.setState({
-            name: event.target.value
-        })
-    
-    }
-    handleOnSubmit = (event) =>{
-        event.preventDefault();
-        alert('me')
-    }
+
     // JSX
     render() {
         return (
-            <div> My name is {this.state.name} and I'm {this.state.age} yearolds
-                <form onSubmit={(event) => this.handleOnSubmit(event)}>
-                    <input 
-                    type="text"
-                    onChange={(event) => this.handleOnChangeInput(event)} 
-                    />
-                    <button>Submit</button>
-                </form>
+                <div>
+                <UserInfor/>
             </div>
            
         );
