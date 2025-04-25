f520bf3932cce94c4e4e1a654c5baa80583997d2
Ta Quang <justicepencil@gmail.com>
2025-04-25 10:16:36 +0700

    bai 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+export default class MyComponent extends React.Component {
+    // JSX
+    render() {
+        return (
+            <div> My first component </div>
+        );
+    }
+}
