73b4507a672207d71dd2f63fa38eea1aa1fbfdbb
Ta Quang <justicepencil@gmail.com>
2025-04-27 00:13:57 +0700

    Hoc conditional

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,27 +1,36 @@
 import React from 'react'
 
 class DisplayInfor extends React.Component {
-
-    render ()
-    {   //destructuring array/object
+    state = {
+        isShowListUser: true 
+    }
+    handleShowHide = () => {
+        this.setState({
+            isShowListUser: !this.state.isShowListUser
+        })
+    }
+    render() {
         const {listUsers} = this.props;
-        console.log(listUsers)
         return(
             <div>
-    
-                {
-                    listUsers.map((user) => {
-                    
-                        return (
-                            <div key={user.id}>
-                                <div>My name's {user.name} </div>
-                                <div>My age is {user.age}</div>
-                            </div>
-                        )
-                    })
-                }
+                <div>
+                    <span onClick={() => this.handleShowHide()}>
+                        {this.state.isShowListUser === true? "Hide" : "Show"} list users:
+                    </span>
+                </div>
+                {this.state.isShowListUser && (
+                    <div>
+                        {listUsers.map((user) => {
+                            return (
+                                <div key={user.id} className={user.age > 18 ? "green" : "red"}>
+                                    <div>My name's {user.name}</div>
+                                    <div>My age is {user.age}</div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                )}
             </div>
-
         )
     }
 }
