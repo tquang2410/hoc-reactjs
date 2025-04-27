466bf22258f40b228454972db16bac0c0eb10f2e
Ta Quang <justicepencil@gmail.com>
2025-04-27 18:07:26 +0700

    Hoc props

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponent from './components/MyComponent';
-import React from 'react'
-export default class App extends React.Component {
-  state = {
-    name: 'Quang',
-    address: 'Hoi Dan It',
-    age: 24
-  }
-  render () {
-    return (
-      <div>
-        <MyComponent></MyComponent>
-      </div>
-    )
-  }
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent />
+    </div>
+  );
 }
+
+export default App;
 
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,30 +1,35 @@
 import React from 'react';
-import UserInfor from './UserInfor';
-import DisplayInfor from './DisplayInfo';
-export default class MyComponent extends React.Component {
+import AddUserInfor from './AddUserInfor'
+import DisplayInfor from './DisplayInfor';
+
+class MyComponent extends React.Component {
     state = {
         listUsers: [
-            {id: 1, name : "Ta Quang", age: "24"},
-            {id: 2, name : "Ket", age: "8"},
-            {id: 3, name : "Linga guli guli", age: "9999"}
+            {id: 1, name: "Ta Quang", age: "24"},
+            {id: 2, name: "Ket", age: "8"},
+            {id: 3, name: "Linga guli guli", age: "9999"}
         ]
     }
-    // JSX
+    handleAddNewUser = (userOjb) => {
+       
+        this.setState({
+            listUsers: [userOjb,...this.state.listUsers]
+        })
+     
+    }
     render() {
-        const myInfor = ['ab','c','d']
         return (
-            //DRY: don't repeat yourself
-                <div>
-                <UserInfor/>
+            <div>
+                <AddUserInfor
+                    handleAddNewUser = {this.handleAddNewUser}
+                />
                 <br /><br />
                 <DisplayInfor 
-                listUsers={this.state.listUsers}
-                
-
+                    listUsers={this.state.listUsers}
+                    
                 />
-           
             </div>
-           
         );
     }
 }
+export default MyComponent
