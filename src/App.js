894fe569a1141a99344ce6a10193cf5fa04a1c31
Ta Quang <justicepencil@gmail.com>
2025-05-03 23:31:22 +0700

    Them router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.scss';
 import Header from './components/Header/Header';
-
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className='App'>
       <Header/>
-      
+      <div>
+        Test Link
+        <div>
+       
+          <button>
+          <Link to="/users">User Page</Link> 
+          </button>
+          <button>
+          <Link to="/admins">Admin Page</Link> 
+          </button>
+        </div>
+      </div>
  
     </div>
   )
