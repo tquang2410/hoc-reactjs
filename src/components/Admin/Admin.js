88948692d0e332cdf2ad86e2d136575ae3bc9c75
Ta Quang <justicepencil@gmail.com>
2025-05-04 21:21:33 +0700

    Bai 43

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,3 +1,4 @@
+import React from 'react'
 const Admin = (props) => {
     return (
         <div>Admin component</div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,10 +3,10 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { NavLink } from "react-router-dom";
-
+import React from 'react'
 const Header = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar bg="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="#home">Hoc React</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
