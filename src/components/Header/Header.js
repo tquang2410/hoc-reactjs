52857edcedf6fc00f7ee70ddf57934f997bb54da
Ta Quang <justicepencil@gmail.com>
2025-05-04 21:43:22 +0700

    Them index route

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,8 @@ const Header = () => {
   return (
     <Navbar bg="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Hoc React</Navbar.Brand>
+        {/* <Navbar.Brand href="#home">Hoc React</Navbar.Brand> */}
+        <NavLink to="/" className='navbar-brand'>Wang hoc react</NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -19,11 +20,9 @@ const Header = () => {
           </Nav>
           <Nav>
           <NavDropdown title="Settings" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Log Out
-              </NavDropdown.Item>
-                <NavDropdown.Item href='#action/3.1'>Profile</NavDropdown.Item>
+              <NavDropdown.Item >Log in</NavDropdown.Item>
+              <NavDropdown.Item > Log Out  </NavDropdown.Item>
+                <NavDropdown.Item >Profile</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
