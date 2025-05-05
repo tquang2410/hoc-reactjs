6933c89077acb71252590c690563923b23553304
Ta Quang <justicepencil@gmail.com>
2025-05-05 17:24:00 +0700

    Them video

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,8 +1,15 @@
-const HomePage = (props) => {
+import React from 'react';
+import videoHomepage from '../../assets/video-homepage.mp4';
+
+const HomePage = () => {
     return (
         <div className="homepage-container">
-                home page component
+            <video className="homepage-video" autoPlay loop muted >
+                <source src={videoHomepage} type="video/mp4" />
+               
+            </video>
         </div>
-    )
+    );
 }
+
 export default HomePage;
